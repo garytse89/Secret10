dfaf91f14b7f9e1b466da23563feb83083342ad9
Gary Tse <garytse89@gmail.com>
2014-09-08 23:46:21 -0700

    Simple ListView after you "Login"

--- src/com/example/secret10/SecretListActivity.java
+++ src/com/example/secret10/SecretListActivity.java
@@ -3,20 +3,41 @@ package com.example.secret10;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class SecretListActivity extends Activity {
+
+
+public class SecretListActivity extends Activity  {
 	
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.register);
-		TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
-		loginScreen.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				finish();
-			};
+		setContentView(R.layout.secretlist);
+		
+		final ListView listView = (ListView) findViewById(R.id.list);
+		
+		// populate list view with array of strings
+		String[] values = new String[]{ "jyj", "micky", "yoochun" };
+		
+		// define an adapter and attach it to listview
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, values);
+		listView.setAdapter(adapter);
+		
+		// set listener on listView
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				 Toast.makeText(getApplicationContext(),
+						 (String) listView.getItemAtPosition(position), Toast.LENGTH_LONG)
+	                      .show();
+			}
 		});
 	}
-
+	
 }
