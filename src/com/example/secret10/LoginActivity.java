6aa6363a0315e4e94330a829170f5d5c310c7231
Gary Tse <garytse89@gmail.com>
2014-09-08 21:57:12 -0700

    Inserted a login function, which is a basic HTTP GET operation (haven't implemented POST on server side yet) It must run from inside a thread, you cannot have Network IO on main thread

--- src/com/example/secret10/LoginActivity.java
+++ src/com/example/secret10/LoginActivity.java
@@ -1,15 +1,31 @@
 package com.example.secret10;
 
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class LoginActivity extends ActionBarActivity {
-
+public class LoginActivity extends Activity {
+	
+	
+	private final String LOGIN_URL = "http://192.168.1.68:3000/login";
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,6 +37,26 @@ public class LoginActivity extends ActionBarActivity {
 			public void onClick(View v) {
 				Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
 				startActivity(i);				
+			}
+		});
+		
+		Button loginBtn = (Button) findViewById(R.id.btnLogin);
+		loginBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v){			
+				Thread thread = new Thread(new Runnable(){
+				    @Override
+				    public void run() {
+				        try {
+				        	login();
+				        } catch (Exception e) {
+				            e.printStackTrace();
+				        }
+				    }
+				});
+
+				thread.start(); 
 			}
 		});
 	}
@@ -42,5 +78,22 @@ public class LoginActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	// defined methods
+	public void login() {
+		Log.d("login", "login() inside");
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet get = new HttpGet(LOGIN_URL);
+		ResponseHandler<String> handler = new BasicResponseHandler();  
+        try {  
+            String result = httpclient.execute(get, handler);  
+            Log.d("login", result);
+        } catch (ClientProtocolException e) {  
+            e.printStackTrace();  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        }  
+        httpclient.getConnectionManager().shutdown();
 	}
 }
