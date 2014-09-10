52f0a647dd55dd8d5c8c449b0b65d9aaa09974b4
Gary Tse <garytse89@gmail.com>
2014-09-09 23:39:32 -0700

    Finally implemented WebSocket client within app Failed with: - socket.io (had to revert to version 0.9, which is poorly documented, too much hassle) - GCM (poor documentation, Python CCS would not even work with Google App Engine..)
    Current implementation =
    Node.js + WebSocket (npm install ws)
    https://github.com/einaros/ws
    Android + Java-WebSocket
    https://github.com/TooTallNate/Java-WebSocket

--- src/com/example/secret10/SecretListActivity.java
+++ src/com/example/secret10/SecretListActivity.java
@@ -1,16 +1,40 @@
 package com.example.secret10;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecretListActivity extends Activity {
 	
+	private WebSocketClient mWebSocketClient;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.register);
+		setContentView(R.layout.sampleclient);
+		
+		connectWebSocket();
+		
+		Button sendBtn = (Button) findViewById(R.id.sendBtn);
+		sendBtn.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				sendMessage();				
+			}
+		});
+		
 		TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
 		loginScreen.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
@@ -18,5 +42,57 @@ public class SecretListActivity extends Activity {
 			};
 		});
 	}
+	
+	private void connectWebSocket() {
+		Log.i("Websocket", "initiate");
+		  URI uri;
+		  try {
+		    uri = new URI("ws://192.168.1.68:8080");
+		  } catch (URISyntaxException e) {
+		    e.printStackTrace();
+		    return;
+		  }
+
+		  mWebSocketClient = new WebSocketClient(uri) {
+		    @Override
+		    public void onOpen(ServerHandshake serverHandshake) {
+		      Log.i("Websocket", "Opened");
+		      mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+		    }
+
+		    @Override
+		    public void onMessage(String s) {
+		      final String message = s;
+		      Log.i("Websocket", s);
+		      runOnUiThread(new Runnable() {
+		        @Override
+		        public void run() {
+		          //TextView textView = (TextView)findViewById(R.id.messages);
+		          //textView.setText(textView.getText() + "\n" + message);
+		        }
+		      });
+		    }
+
+		    @Override
+		    public void onClose(int i, String s, boolean b) {
+		      Log.i("Websocket", "Closed " + s);
+		    }
+
+		    @Override
+		    public void onError(Exception e) {
+		      Log.i("Websocket", "Error " + e.getMessage());
+		    }
+		  };
+		  
+		  Log.i("Websocket", "connecting");
+		  mWebSocketClient.connect();
+		}
+	
+	public void sendMessage() {
+		EditText editText = (EditText)findViewById(R.id.message);
+		mWebSocketClient.send(editText.getText().toString());
+		editText.setText("");
+	}
+	
 
 }
